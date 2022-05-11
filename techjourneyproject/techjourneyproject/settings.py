6484e00d9e491e95fc6dfd0e644067c1160ccd7e
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2022-05-11 11:45:49 -0700

    Added fontawesome package, added icons to memberDashboard

--- techjourneyproject/techjourneyproject/settings.py
+++ techjourneyproject/techjourneyproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard' #my app is registered in django project
+    'fontawesomefree',
+    'dashboard', #my app is registered in django project,
 ]
 
 #Use the new CustomUser class as Auth
